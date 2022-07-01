2dda061f69e7a1b2396eafc76f15cfe0c4b23e94
lijo-harness <108330094+lijo-harness@users.noreply.github.com>
2022-07-01 08:57:38 +0530

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
    backend "s3" {
-   bucket = "rc-tf-remote-state-bucket"
+   bucket = "rc-tf-remote-state-bucket-lijo"
    key = "terraform.tfstate"
    region = "us-east-1"
   }
